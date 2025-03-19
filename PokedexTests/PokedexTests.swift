d01f3ec8c8eb2b73091d9c2c25138e8ad2065ea3
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-03-19 11:24:41 -0300

    Initial Commit

---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by Felipe Ribeiro on 19/03/25.
+//
+
+import Testing
+@testable import Pokedex
+
+struct PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
