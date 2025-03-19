2bdb815905c7c26758fd8dc027dc8b9cc749e321
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-03-19 13:22:15 -0300

    Add basic gauges for element types

--- Pokedex/Views/StatisticsView.swift
+++ Pokedex/Views/StatisticsView.swift
@@ -7,24 +7,56 @@
 
 import SwiftUI
 
+struct TypeData { // TODO: dict?
+    let type: ElementType
+    
+    var total: Double {
+        Double(pokemons.count { pokemon in
+            pokemon.types.contains(type)
+        })
+    }
+    
+    var caught: Double {
+        Double(pokemons.count { pokemon in
+            pokemon.types.contains(type) && pokemon.isCaught
+        })
+    }
+}
+
 struct StatisticsView : View {
     private var numberOfPokemon = Double(pokemons.count)
     private var caughtCount = Double(pokemons.count(where: { $0.isCaught } ))
-
-
+    
+    
     var body: some View {
-        Gauge(value: caughtCount, in: 0...numberOfPokemon) {
-            Text("Capturados")
-        } currentValueLabel: {
-//            TODO: String interpolation vs. format:?
-//            Text("\(Int(caughtCount))")
-            Text(caughtCount, format: .number)
-        } minimumValueLabel: {
-//            Text("\(Int(0))")
-            Text(0, format: .number)
-        } maximumValueLabel: {
-//            Text("\(Int(numberOfPokemon))")
-            Text(numberOfPokemon, format: .number)
+        List {
+            //            TODO: Gauge labels - string interpolation vs. format:?
+            Gauge(value: caughtCount, in: 0...numberOfPokemon) {
+                Text("Total")
+            } currentValueLabel: {
+                //            Text("\(Int(caughtCount))")
+                Text(caughtCount, format: .number)
+            } minimumValueLabel: {
+                //            Text("\(Int(0))")
+                Text(0, format: .number)
+            } maximumValueLabel: {
+                //            Text("\(Int(numberOfPokemon))")
+                Text(numberOfPokemon, format: .number)
+            }
+            
+            ForEach(ElementType.allCases, id: \.self) { type in
+                let data = TypeData(type: type)
+                
+                Gauge(value: data.caught, in: 0...data.total) {
+                    Text(type.rawValue.capitalized)
+                } currentValueLabel: {
+                    Text(data.caught, format: .number)
+                } minimumValueLabel: {
+                    Text(0, format: .number)
+                } maximumValueLabel: {
+                    Text(data.total, format: .number)
+                }
+            }
         }
     }
 }
