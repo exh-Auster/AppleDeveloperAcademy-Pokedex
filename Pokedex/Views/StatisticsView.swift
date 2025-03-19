15f1b47fb834195f0d55ef6c364a7147e7bf2c25
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-03-19 12:26:46 -0300

    Add basic Gauge for caught Pokemon

--- Pokedex/Views/StatisticsView.swift
+++ Pokedex/Views/StatisticsView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct StatisticsView : View {
-    var body : some View {
-//        Text("Estatisticas")
-        Text("Caught: \(pokemons.count(where: { $0.isCaught }))")
+    private var numberOfPokemon = Double(pokemons.count)
+    private var caughtCount = Double(pokemons.count(where: { $0.isCaught } ))
+
+
+    var body: some View {
+        Gauge(value: caughtCount, in: 0...numberOfPokemon) {
+            Text("Capturados")
+        } currentValueLabel: {
+//            TODO: String interpolation vs. format:?
+//            Text("\(Int(caughtCount))")
+            Text(caughtCount, format: .number)
+        } minimumValueLabel: {
+//            Text("\(Int(0))")
+            Text(0, format: .number)
+        } maximumValueLabel: {
+//            Text("\(Int(numberOfPokemon))")
+            Text(numberOfPokemon, format: .number)
+        }
     }
 }
 
 #Preview {
     StatisticsView()
 }
+
+/*
+ https://developer.apple.com/design/human-interface-guidelines/gauges
+ https://developer.apple.com/documentation/swiftui/gauge
+ 
+ https://www.hackingwithswift.com/read/0/5/string-interpolation
+ https://developer.apple.com/documentation/swiftui/text/init(_:format:)
+ */
