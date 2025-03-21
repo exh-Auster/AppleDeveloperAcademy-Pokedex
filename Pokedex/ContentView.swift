53e6df5c4c5e80fffb51479aa660d60627e7821e
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-03-21 10:22:57 -0300

    Add profile picker sheet

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var store = PokemonStore()
+    @State var isPresented = true
+    @State var user: User = .ash
     
     var body: some View {
 //        TabView {
@@ -32,6 +34,82 @@ struct ContentView: View {
                 }
         }
         .environmentObject(store)
+        .sheet(isPresented: $isPresented) {
+            VStack(spacing: 20) {
+                let caughtPokemons = store.pokemons.filter { user.presetCaughtPokemons.contains($0.id) }
+                let randomPokemonID = caughtPokemons.randomElement()?.id ?? 1
+                
+                Image(String(randomPokemonID))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                //                    .clipShape(Circle())
+                //                    .shadow(radius: 5)
+                
+                Text("Welcome back!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                VStack(spacing: 15) {
+                    ForEach(User.allCases, id: \.self) { user in
+                        Button(action: { self.user = user }) {
+                            HStack {
+                                ZStack {
+                                    Circle()
+                                        .fill(Color.blue)
+                                        .frame(width: 50, height: 50)
+                                    
+                                    if user == .ash {
+                                        Image("ash")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 50, height: 50)
+                                            .clipShape(Circle())
+                                    } else {
+                                        Text(String(user.rawValue.prefix(1)).uppercased())
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                                .padding(.trailing, 6)
+                                
+                                Text(user.rawValue.capitalized)
+                                    .font(.headline)
+                                    .foregroundColor(user == self.user ? .blue : .primary)
+                                
+                                Spacer()
+                                
+                                if user == self.user {
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .padding()
+                            .background {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color(.systemGray6))
+                            }
+                        }
+                    }
+                }
+                .padding()
+                
+                Button {
+                    store.selectedUser = user
+                    isPresented = false
+                } label: {
+                    Text("Log in")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                        .shadow(radius: 3)
+                }
+                .padding(.horizontal)
+            }
+            .padding()
+        }
     }
 }
 
